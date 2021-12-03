42f8edbb1f6e6ad54b7cf42e5e2cfa758d48bafd
Christian Soto <christian.j.sotoparra@gmail.com>
2021-12-02 23:39:46 -0700

    Connecting GUI Widgets with Functions

--- script.py
+++ script.py
@@ -3,11 +3,18 @@ from  tkinter import *
 #create window
 window=Tk()
 
+def km_to_miles():
+	miles=float(e1_value.get())*1.6
+	t1.insert(END, miles)
+
 #add button, entry, and text widgets to window
-b1=Button(window, text="Execute")
+b1=Button(window, text="Execute", command=km_to_miles)
 b1.grid(row=0,column=0)
 
-e1=Entry(window)
+#Create string entry object to be used by entry widget
+e1_value=StringVar()
+
+e1=Entry(window, textvariable=e1_value)
 e1.grid(row=0,column=1)
 
 t1=Text(window, height=1, width=20)
